d1b8683db1fccf5ca0e30eebb004dd2fdefbde05
Vereshan Veron Palani <ST10025632@vcconnect.edu.za>
2024-09-23 14:52:53 +0200

    Made changes to fragment_profile.xml: I added a View for profile attributes such as Name , email and profile image. I added 5 cardViews: cvNotifications, cvLanguage, cvReportBugs, cvAboutDevelopers, cvLogout. Each containing similar components(image& label)

--- app/src/main/java/za/co/varsitycollege/st10215473/pank/ProfileFragment.kt
+++ app/src/main/java/za/co/varsitycollege/st10215473/pank/ProfileFragment.kt
@@ -34,15 +34,7 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity).setDrawerEnabled(true)
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        (activity as MainActivity).setDrawerEnabled(false)
-    }
 
     companion object {
         /**
